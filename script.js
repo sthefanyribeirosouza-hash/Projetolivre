a7063c02ce9edb2f80047287c5ca4e800ec7d75c
sthefanyribeirosouza-hash <sthefany.ribeiro.souza@escola.pr.gov.br>
2026-08-11 19:04:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function responder(resposta) {
 
+    const resultado = document.getElementById("resultado");
+
+    if (resposta === true) {
+
+        resultado.innerHTML =
+            "✅ Correto! A homofobia não acontece apenas por meio de violência física. Ela também pode aparecer em palavras, atitudes, exclusão, discriminação e violência psicológica.";
+
+        resultado.style.color = "green";
+
+    } else {
+
+        resultado.innerHTML =
+            "❌ Pense novamente! A homofobia pode aparecer de diversas formas, não apenas por meio de violência física.";
+
+        resultado.style.color = "red";
+
+    }
+
+}
